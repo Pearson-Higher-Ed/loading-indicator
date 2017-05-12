40f73ea775471334b2d80a79f9e2bc454f96a4a0
Jonathan Wyse <jonathan.wyse@pearson.com>
2017-05-12 15:55:01 -0600

    loading indicator centered h/v in div

--- src/js/component-owner.js
+++ src/js/component-owner.js
@@ -10,24 +10,30 @@ class ComponentOwner extends Component {
 
     super(props);
     this.state = {
-      containerHeight: 0
+      containerHeight: 0,
+      chipVertPos: 0
     };
   }
 
   componentDidMount() {
-    this.setState({containerHeight: ReactDOM.findDOMNode(this).parentNode.clientHeight});
-    console.log(this.state.containerHeight);
+    const chipHeight = 70;
+    const chipVertPos = Math.floor((ReactDOM.findDOMNode(this).parentNode.clientHeight - chipHeight) / 2);
+    this.setState({
+      containerHeight: ReactDOM.findDOMNode(this).parentNode.clientHeight,
+      chipVertPos: chipVertPos
+    });
+    console.log(this.state.containerHeight + ' ' + this.state.chipVertPos);
   }
 
   render() {
-    console.log('render invoked with ' + this.state.containerHeight);
+    console.log('render invoked with ' + this.state.containerHeight + ' and ' + this.state.chipVertPos);
     const { appLevel, data } = this.props;
     const overlayStyle = appLevel ? 'pe-loadingIndicator-overlay pe-loadingIndicator-overlay-app' : 'pe-loadingIndicator-overlay';
 
     return (
       <div className="pe-loadingIndicator">
         <div className={overlayStyle} style={{height: this.state.containerHeight + 'px'}}>
-          <div className="pe-loadingIndicator-chip">
+          <div className="pe-loadingIndicator-chip" style={{top: this.state.chipVertPos}}>
             <div className="pe-loadingIndicator-spinner"></div>
             <div className="pe-loadingIndicator-chip-text">{data.text.chipText}</div>
           </div>
