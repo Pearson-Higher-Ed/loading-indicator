6997008b195574022fe25b0c8f4dad709bd4d1e6
Jonathan Wyse <jonathan.wyse@pearson.com>
2017-12-06 09:45:07 -0700

    fix: clear warnings for unit tests

--- test/component-owner.test.js
+++ test/component-owner.test.js
@@ -3,7 +3,7 @@
 import expect                from 'expect';
 import expectJSX             from 'expect-jsx';
 import React                 from 'react';
-import TestUtils             from 'react-addons-test-utils';
+import TestUtils             from 'react-dom/test-utils';
 import { LoadingIndicator }  from '../index';
 import { shallow }           from 'enzyme';
 
